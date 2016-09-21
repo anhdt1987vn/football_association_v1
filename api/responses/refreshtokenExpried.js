c2052d270386d094a8f2d5000303696d685916c1
freeman <opensource1987@gmail.com>
2016-09-22 00:09:59 +0700

    add response about refresh token case

---
+++ api/responses/refreshtokenExpried.js
@@ -0,0 +1,22 @@
+/**
+ * 700 (Refresh Token Is Expried) Response
+ *
+ * The request has been fulfilled and resulted in a new resource being created.
+ * Successful creation occurred (via either POST or PUT).
+ * Set the Location header to contain a link to the newly-created resource (on POST).
+ * Response body content may or may not be present.
+ */
+
+module.exports = function (data, code, message, root) {
+	console.log("CODE: " + code + "__" + "MESSAGE: " + message + "__" + "ROOT: " + root + "__" + "This: " + this + "__" + "Data: " + data);
+    var response = _.assign({
+        code: code || 'REFRESH_TOKEN_IS_EXPRIED',
+        message: message || 'Your refresh token is expried, please login again',
+        data: data || {}
+    }, root);
+
+    this.req._sails.log.silly('Sent (700 REFRESH_TOKEN_IS_EXPRIED)\n', response);
+
+    this.res.status(201);
+    this.res.jsonx(response);
+};
