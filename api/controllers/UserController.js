cb19d09543a97d91372639bc61ecf6cbf7710851
freeman <opensource1987@gmail.com>
2016-09-26 23:37:09 +0700

    add debug zone for debug/test

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -3,4 +3,34 @@
  * @description :: Server-side logic for manage users
  */
 
-module.exports = {};
+module.exports = {
+
+
+	/**start_DEBUG_ZONE */
+	
+	/**
+	 * [getByID get informations of User by ID]
+	 * @param  {[type]} req [description]
+	 * @param  {[type]} res [description]
+	 * @return {[user]}     [information of User]
+	 */
+	getByID: function(req, res){
+		var userID = req.param('user_id');
+
+		User.findOne({id: userID})
+			.exec(function(err, user){
+				if(err){
+					return res.serverError(err);
+				}
+				if(!user){
+					return res.notFound('Could not find user');
+				}
+				return res.ok({
+					user: user
+				});
+			});
+	}
+
+	/**end_DEBUG_ZONE */
+
+};
